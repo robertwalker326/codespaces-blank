6066395172cbfdfc727c93a4f211788d4092a190
rob46 <rwalker070@boyslatin.net>
2026-09-01 15:05:43 +0000

    first commit

---
+++ unit 1/datatypes.py
@@ -0,0 +1,36 @@
+# String-characters that are wrapped within qoutes.
+# characters can be letters,numbers,and symbols
+# you can use both single and double quotations
+# marks for strings.
+
+"Robert walker"
+
+'123 main street'
+
+'this cost $100 dollars'.
+
+# the hashtag is a marker that makes anything written
+# behind it invisible to the computer
+
+
+# Intergers-datatypes that are just whole numbers,
+# or numbers without a decimal point. we dont have
+# to add any special symbols.we can write the number
+# as is.
+
+10
+20
+2000000
+
+# Float-a number with a decimal.
+20.99
+3.339
+0.23
+
+# Boolean-a data type that can only be either
+# true or false we do not use qouatation marks for
+# boolean values.the computer understand these words 
+# as something unique.
+
+False
+True
